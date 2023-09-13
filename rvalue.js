0e30d0c4895c0068e124ae6ba03c9b656b08ea3f
mahi028 <116808894+mahi028@users.noreply.github.com>
2023-09-13 15:39:48 +0530

    Add files via upload

---
+++ rvalue.js
@@ -0,0 +1,57 @@
+let listcolor=[
+    'Black',
+    'Brown',
+    'Red', 
+    'Orange', 
+    'Yellow', 
+    'Green', 
+    'Blue', 
+    'Violet', 
+    'Gray', 
+    'White', 
+    'Gold', 
+    'Silver', 
+    'None'
+]
+
+const multiplyer={
+    0:1,
+    1:10,
+    2:10**2,
+    3:10**3,
+    4:10**4,
+    5:10**5,
+    6:10**6,
+    7:10**7,
+    8:10**8,
+    9:10**9,
+    10:10**-1,
+    11:10**-2
+}
+
+const tolerance={
+    0:'&plusmn;20%',
+    1:'&plusmn;1%',
+    2:'&plusmn;2%',
+    3:'&plusmn;3%',
+    4:'&plusmn;4%',
+    5:'&plusmn;0.5%',
+    6:'&plusmn;0.25%',
+    7:'&plusmn;0.1%',
+    8:'&plusmn;0.05%',
+    9:'&plusmn;10%',
+    10:'&plusmn;5%',
+    11:'&plusmn;10%'
+}
+
+const TCR={
+    0:'250 ppm/K',
+    1:'100 ppm/K',
+    2:'50 ppm/K',
+    3:'15 ppm/K',
+    4:'25 ppm/K',
+    5:'20 ppm/K',
+    6:'10 ppm/K',
+    7:'5 ppm/K',
+    8:'1 ppm/K'
+}
---
+++ script.js
@@ -0,0 +1,89 @@
+const menuitems = document.querySelectorAll('.menu>li');
+const menu=document.querySelector('.menu');
+const selectedband=document.querySelectorAll('.band');
+const output=document.querySelector('.value');
+let bandnum=null;
+let bandvalue=new Object;
+let totalband;
+
+
+
+
+//assigning colors to menu items
+let i=0;
+menuitems.forEach(item => {
+    if(listcolor[i]=='Black'){
+        item.style.backgroundColor='gray';
+    }
+    else if(listcolor[i]=='Violet'){
+        item.style.backgroundColor='#8F00FF';
+    }
+    else{
+        item.style.backgroundColor=listcolor[i];
+    }
+    i+=1;
+});
+
+//displaying required number of bands
+function numofband(){
+    totalband= document.getElementById('inputt').value
+    for(band of selectedband){
+        band.style.display='none';
+    }
+    for(let j=0;j<totalband;j++){
+        selectedband[j].style.display='block';
+    }
+}
+//selecting band and displaying menu
+function dropmenu(x){
+    bandnum=x;
+    menu.style.display='block';
+    for(y of selectedband){
+        y.style.width='30px';
+    }
+    selectedband[x].style.width='35px';
+}
+//changing color of band
+function changecolor(x){
+    bandvalue[bandnum]=x; 
+    if(listcolor[x]=='None'){
+        selectedband[bandnum].style.backgroundColor='#ffd70000';
+    }
+    else{
+    selectedband[bandnum].style.backgroundColor=listcolor[x];
+}}
+
+//calculating resistance
+function calculate(){
+    if(totalband==1){
+        output.innerHTML='0 ohms';
+    }
+    else if(totalband==3){
+        let number=''+bandvalue[0]+bandvalue[1];
+        let m=multiplyer[bandvalue[2]];
+        output.innerHTML=`${number*m}&#8486; ${tolerance[0]}`;
+    }
+    else if(totalband==4){
+        let number=''+bandvalue[0]+bandvalue[1];
+        let m=multiplyer[bandvalue[2]];
+        output.innerHTML=`${number*m}&#8486; ${tolerance[bandvalue[3]]}`;
+    }
+    else if(totalband==5){
+        if(bandvalue[3]==10 || bandvalue[3]==11){
+            let number=''+bandvalue[0]+bandvalue[1];
+            let m=multiplyer[bandvalue[2]];
+            output.innerHTML=`${number*m}&#8486; ${tolerance[bandvalue[3]]}, ${TCR[bandvalue[4]]}`;
+        }
+
+        else{
+            let number=''+bandvalue[0]+bandvalue[1]+bandvalue[2];
+            let m=multiplyer[bandvalue[3]];
+            output.innerHTML=`${number*m}&#8486; ${tolerance[bandvalue[4]]}`;
+        }
+    }
+    else if(totalband==6){
+        let number=''+bandvalue[0]+bandvalue[1]+bandvalue[2];
+        let m=multiplyer[bandvalue[3]];
+        output.innerHTML=`${number*m}&#8486; ${tolerance[bandvalue[4]]}, TCR=${TCR[bandvalue[5]]}`;
+    }
+}
